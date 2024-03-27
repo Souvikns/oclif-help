2f2a0d864169ddd88b4c5f755866f554a7f1dd37
souvik <souvikde.ns@gmail.com>
2024-03-27 11:14:41 +0530

    feat:add topics to commands list

---
+++ src/commands/config/create.ts
@@ -0,0 +1,7 @@
+import {Command} from '@oclif/core'
+
+export default class CreateConfigClass extends Command {
+    run(): Promise<any> {
+        throw new Error('Method not implemented.');
+    }
+}
--- src/customHelp.ts
+++ src/customHelp.ts
@@ -1,8 +1,7 @@
 import { Command, Help } from '@oclif/core'
 import { SINGLE_COMMAND_CLI_SYMBOL } from '@oclif/core/lib/symbols'
-import { Config } from '@oclif/core/lib/interfaces'
+import { Config, Topic } from '@oclif/core/lib/interfaces'
 import { colorize } from '@oclif/core/lib/cli-ux'
-import stripAnsi from 'strip-ansi'
 
 export default class CustomHelp extends Help {
   async showHelp(argv: string[]): Promise<void> {
@@ -32,10 +31,6 @@ export default class CustomHelp extends Help {
     }
   }
 
-  async showCommandHelp(command: Command.Loadable): Promise<void> {
-    console.log(command.id)
-  }
-
   async showRootHelp(): Promise<void> {
     let rootTopics = this.sortedTopics
     let rootCommands = this.sortedCommands
@@ -57,36 +52,25 @@ export default class CustomHelp extends Help {
       rootCommands = rootCommands.filter((c) => !c.id.includes(':'))
     }
 
-    if (rootTopics.length > 0) {
-      this.log(this.formatTopics(rootTopics))
-      this.log('')
-    }
+    // if (rootTopics.length > 0) {
+    //   this.log(this.formatTopics(rootTopics))
+    //   this.log('')
+    // }
 
-    if (rootCommands.length > 0) {
+    if (rootCommands.length > 0 && rootTopics.length > 0) {
       rootCommands = rootCommands.filter((c) => c.id)
-      this.log(this.formatCommands(rootCommands))
+      this.log(this.formatCommandsAndTopics(rootCommands, rootTopics))
       this.log('')
     }
   }
-  protected formatCommands(commands: Command.Loadable[]): string {
-    if (commands.length === 0) return ''
-    // const body = this.renderList(
-    //   commands
-    //   .filter((c) => this.opts.hideAliasesFromRoot? !c.aliases?.includes(c.id): true)
-    //   .map((c)=> {
-    //     if (this.config.topicSeparator !== ':') c.id = c.id.replaceAll(':', this.config.topicSeparator)
-    //     const summary = this.summary(c)
-    //   return [
-    //     colorize(this.config?.theme?.command, c.id),
-    //     summary && colorize(this.config?.theme?.sectionDescription, stripAnsi(summary))
-    //   ]
-    //   }),{
-    //     indentation: 2,
-    //     spacer: '\n',
-    //     stripAnsi: this.opts.stripAnsi
-    //   }
-    // )
-    const body = this.renderList(
+
+  protected formatCommandsAndTopics(
+    commands: Command.Loadable[],
+    topics: Topic[]
+  ): string {
+    if (commands.length === 0 && topics.length === 0) return ''
+
+    let body = this.renderList(
       commands
         .filter((c) =>
           this.opts.hideAliasesFromRoot ? !c.aliases?.includes(c.id) : true
@@ -96,9 +80,26 @@ export default class CustomHelp extends Help {
             c.id.replaceAll(':', this.config.topicSeparator)
           const summary = this.summary(c)
           return [colorize('magenta', c.id), summary]
-        }),
+        })
+        .concat(
+          topics.map((c) => {
+            if (this.config.topicSeparator !== ':')
+              c.name = c.name.replaceAll(':', this.config.topicSeparator)
+            return [
+              colorize(this.config?.theme?.topic || 'cyan', c.name),
+              c.description &&
+                this.render(
+                  colorize(
+                    this.config?.theme?.sectionDescription,
+                    c.description.split('\n')[0]
+                  )
+                ),
+            ]
+          })
+        ),
       { indentation: 2, spacer: '\n', stripAnsi: this.opts.stripAnsi }
     )
+
     return this.section(colorize('cyan', 'COMMANDS'), body)
   }
 }
